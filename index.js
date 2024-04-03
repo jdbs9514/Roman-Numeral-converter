314b8db07004cf700f46fe3918669f2273b1c5ca
Josue Benavides <jdbs9514@gmail.com>
2024-04-03 13:56:24 -0500

    start with js file

---
+++ index.js
@@ -0,0 +1,23 @@
+const input = document.querySelector('input');
+const convertBtn = document.getElementById('convert-btn');
+
+let romanNum = [{
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1
+}]
+
+const convertToRoman = (num) => {
+  console.log(num)
+}
+convertBtn.addEventListener('click', convertToRoman);

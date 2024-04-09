8adc96a31e18f1c31c15bc316a45d53c45671d60
Josue Benavides <jdbs9514@gmail.com>
2024-04-09 18:27:42 -0500

    done exercise

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const input = document.querySelector('input');
-const convertBtn = document.getElementById('convert-btn');
-const outPut = document.getElementById('output');
-
+const input = document.querySelector("input");
+const convertBtn = document.getElementById("convert-btn");
+const outPut = document.getElementById("output");
 
 const convertToRoman = (num) => {
-
   let romanNum = {
     1: "I",
     4: "IV",
@@ -18,24 +16,48 @@ const convertToRoman = (num) => {
     400: "CD",
     500: "D",
     900: "CM",
-    1000: "M"
+    1000: "M",
   };
 
-  if (num <= 0){
-    const p = document.createElement('p');
-    p.textContent = "Please enter a number greater than 0";
-    outPut.appendChild(p);
-  }
-
-  for (let n of Object.keys(romanNum).sort((a,b) => b - a)){
-    n = parseInt(n);
-    if (num >= n){
-      return romanNum[n] + convertToRoman(num - n);
+    for (let n of Object.keys(romanNum).sort((a, b) => b - a)) {
+      n = parseInt(n);
+      if (num >= n) {
+        return romanNum[n] + convertToRoman(num - n);
+      }
     }
-  }
+
+    if (num === 0) {
+      return "";
+    }
   
-}
+    if (num >= 4000) {
+      return "Please enter a number less than 4000";
+    }
+  
+    if (num <= 0) {
+      return "Please enter a number greater than 0";
+    }
+  
+    if (num === "") {
+      return "Please enter a valid number";
+    }
 
+};
 
+convertBtn.addEventListener("click", (e) => {
 
-convertBtn.addEventListener('click', convertToRoman);
+  e.preventDefault();
+  
+  const num = input.value;
+  const result = convertToRoman(num)
+  
+  if (!num) {
+    outPut.innerHTML = "Please enter a valid number";
+  } else if (num <= 0) {
+    outPut.innerHTML = "Please enter a number greater than or equal to 1";
+  } else if (num >= 4000) {
+    outPut.innerHTML = "Please enter a number less than or equal to 3999";
+  } else {
+    outPut.innerHTML = result;
+  }
+});
